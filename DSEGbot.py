9359a07a66a4ef10c79cdb1e8f111cf97aa15ecc
lawrence-jung <86976023+lawrence-jung@users.noreply.github.com>
2021-08-30 12:33:36 -0400

    Update DSEGbot.py

--- DSEGbot.py
+++ DSEGbot.py
@@ -3,55 +3,22 @@ import os
 from datetime import datetime
 from discord.ext import tasks, commands
 from server import keep_alive
+import drive
+from drive import projects, messages, log, Event
+
+#message key
+OPT_IN_S = 0
+OPT_OUT_S = 1
+OPT_IN_F = 2
+OPT_OUT_F = 3
+MILESTONE = 4
 
 client = commands.Bot('.')
 users = []
 to_send = ''
 
-#global variables for lines that stay the same when writing to new file with updated user.id list
-line_two = ''
-line_three = ''
-
 @client.event
 async def on_ready():
-
-    f = open('testinfo.txt','r')
-
-    #LINE ONE: read users from file into list
-    ids = f.readline().split()[1:]
-    for id in ids:
-        user = await client.fetch_user(id)
-        if user is not None:
-            users.append(user)
-    
-    #LINE TWO: read start date, then calculate how many days since project start
-    global line_two
-    line_two = f.readline()
-
-    start_date_str = line_two.split()[1]
-    start_date = datetime.strptime(start_date_str, "%m/%d/%Y")
-
-    now = datetime.now()
-    days_since_start = (now - start_date).days
-
-    #LINE THREE: read milestone days, then see if today is a reminder day - reminders are 7, 3, and 1 days before milestone 
-    #TO DO: get milestone names and pair them with days (there can be notifications for different milestones on the same day)
-    global line_three
-    line_three = f.readline() 
-
-    milestones = line_three.split()[1:]
-    for m in milestones:
-        global to_send
-        days_until_m = int(m) - days_since_start
-        if days_until_m == 1:
-            to_send = to_send + 'Milestone approaching in 1 day.'
-        if days_until_m == 3:
-            to_send = to_send + 'Milestone approaching in 3 days. '
-        if days_until_m == 7:
-            to_send = to_send + 'Milestone approaching in 7 days. '
-
-    f.close()
-
     print('{0.user} reporting for duty'.format(client))
       
 @client.event
@@ -59,93 +26,101 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #opting in, adds user to list of users and adds their id to the file
-    if message.content == '$opt in':
-        if message.author in users:
-            await message.author.send("You have already opted in!")
+       #opting in, adds user to list of users and adds their id to the file
+    if message.content.startswith('$opt in'):
+        msg = message.content.split()
+        args = len(msg)
+        if args != 3:
+            await message.author.send("Invalid # of arguments! Format: \"$opt in [project_name]\"")
         else:
-            users.append(message.author)
-            await message.author.send('You have opted into project milestone reminders! You will be notified 7, 3, and 1 day(s) before every milestone.')
-            update_file()
+            for p in projects:
+                if msg[2] == p.title:
+                    if message.author.id in p.discord_ids:
+                        await message.author.send(messages[OPT_IN_F])
+
+                        now = datetime.now()
+                        log.append(Event(datetime.strftime(now, "%m/%d/%Y %H:%M"), 'user failed to opt into ' + p.title, message.author.name))
+                        drive.update_log(log)
+                    else:
+                        p.discord_ids.append(message.author.id)
+                        await message.author.send(messages[OPT_IN_S])
+                        drive.update_file(p)
+
+                        now = datetime.now()
+                        log.append(Event(datetime.strftime(now, "%m/%d/%Y %H:%M"), 'user successfully opted into ' + p.title, message.author.name))
+                        drive.update_log(log)
+                    return
+            await message.author.send("Project name not found.")
 
     #opting out, removes user from list of users and from file
-    if message.content == '$opt out':
-        if message.author in users:
-            users.remove(message.author)
-            await message.author.send('You have opted out of project milestone reminders. Use \'$opt in\' to opt back in.')
-            update_file()
+    if message.content.startswith('$opt out'):
+        msg = message.content.split()
+        args = len(msg)
+        if args != 3:
+            await message.author.send("Invalid # of arguments! Format: \"$opt out [project_name]\"")
         else:
-            await message.author.send('There is nothing for you to opt out of!')
+            for p in projects:
+                if msg[2] == p.title:
+                    if message.author.id in p.discord_ids:
+                        p.discord_ids.remove(message.author.id)
+                        drive.update_file(p)
+                        await message.author.send(messages[OPT_OUT_S])
+
+                        now = datetime.now()
+                        log.append(Event(datetime.strftime(now, "%m/%d/%Y %H:%M"), 'user successfully opted out of ' + p.title, message.author.name))
+                        drive.update_log(log)
+                    else:
+                        await message.author.send(messages[OPT_OUT_F])
+
+                        now = datetime.now()
+                        log.append(Event(datetime.strftime(now, "%m/%d/%Y %H:%M"), 'user failed to opt out of ' + p.title, message.author.name))
+                        drive.update_log(log)
+                    return
+            await message.author.send("Project name not found.")       
 
 
+#TODO: refresh sheets and create log if it's sunday
 #if today is a reminder day, sends reminder message to opted-in users at desired interval
 @tasks.loop(hours=24)
 async def loop_send():
+  global projects
+  global messages
 
-    #everyday undergo same process in on_ready
-    f = open('testinfo.txt','r')
+  if datetime.today().weekday() == 2:
+    drive.refresh()
+    projects = drive.get_projects()
+    messages = drive.get_messages()
 
-    #LINE ONE: read users from file into list
-    ids = f.readline().split()[1:]
-    for id in ids:
-        user = await client.fetch_user(id)
-        if user is not None:
-            users.append(user)
-    
-    #LINE TWO: read start date, then calculate how many days since project start
-    global line_two
-    line_two = f.readline()
+  for i in range(0, len(projects)):
 
-    start_date_str = line_two.split()[1]
-    start_date = datetime.strptime(start_date_str, "%m/%d/%Y")
+    last_sent = projects[i].last_sent
 
-    now = datetime.now()
-    days_since_start = (now - start_date).days
+    if (datetime.now() - datetime.strptime(last_sent, "%m/%d/%Y")).days >= 1:
 
-    #LINE THREE: read milestone days, then see if today is a reminder day - reminders are 7, 3, and 1 days before milestone 
-    #TO DO: get milestone names and pair them with days (there can be notifications for different milestones on the same day)
-    global line_three
-    line_three = f.readline() 
+        #everyday undergo same process in on_ready
+        start_date = datetime.strptime(projects[i].start_date, "%m/%d/%Y")
 
-    milestones = line_three.split()[1:]
-    global to_send
-    for m in milestones:
-        days_until_m = int(m) - days_since_start
-        if days_until_m == 1:
-            to_send = to_send + 'Milestone approaching in 1 day.'
-        if days_until_m == 3:
-            to_send = to_send + 'Milestone approaching in 3 days. '
-        if days_until_m == 7:
-            to_send = to_send + 'Milestone approaching in 7 days. '
+        now = datetime.now()
+        days_since_start = (now - start_date).days
 
-    f.close()
+        for m in range(0,len(projects[i].m_dates)):
+            days_until_m = projects[i].m_dates[m] - days_since_start
+            if days_until_m == 1 or days_until_m == 3 or days_until_m == 7:
 
-    if to_send != '':
-        for u in users:
-            await u.send(to_send)
+              for id in projects[i].discord_ids:
+                try:
+                  user = await client.fetch_user(id)
+                  await user.send(messages[MILESTONE] + "\nMilestone: " + projects[i].m_names[m] + "\tProject Day: " + str(projects[i].m_dates[m]))
+                except:
+                  #log failure to find id
+                  log.append(Event(datetime.strftime(now, "%m/%d/%Y %H:%M"), 'bot tried to message nonexistent user', 'N/A'))
+                  drive.update_log(log)
+            projects[i].last_sent = datetime.strftime(now, "%m/%d/%Y")
+            drive.update_file(projects[i])
+  
+  
 
-    to_send = ''
-
-#can't edit files, so this behavior is emulated by making a temp file and keeping every line the same except
-#for the new user id list, then setting the old file to the new one
-def update_file():
-    f = open("temp.txt", "w")
-
-    #updated line one
-    f.write("Users: ")
-    for u in users:
-        f.write(str(u.id))
-        f.write(' ')
-    f.write("\n")
-
-    #lines two and three unchanged
-    f.write(line_two)
-    f.write(line_three)
-
-    f.close()
-    os.rename('temp.txt', 'testinfo.txt')
-        
 #run server, activate loop, run bot
 keep_alive()
 loop_send.start()
-client.run('ODU2OTA4MTMzMjc1NjY0Mzk0.YNH34w.rGxJRL8EvYcz8kIJg2KxGO0MdX0')
+client.run('insert bot token here')
